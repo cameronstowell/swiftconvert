afb89f596315663df98cfc77ed4b47fb56ce4746
Cameron Stowell <camerons1016@gmail.com>
2026-01-12 20:30:14 -0800

    Initial Commit

---
+++ Video Converter/Video_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_ConverterApp.swift
+//  Video Converter
+//
+//  Created by Cameron Stowell on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Video_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
